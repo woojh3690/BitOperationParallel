6fd0470eab832e0ff9282f106240cee55227a53b
Jun-Hyuk Woo <woojh3690@naver.com>
2019-08-12 12:25:58 +0900

    기존 연산과 속도 비교

--- BitOperation/BitOperationMain.cpp
+++ BitOperation/BitOperationMain.cpp
@@ -1,42 +1,84 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <bitset>
+#include <time.h>
 using namespace std;
 
 int main(void)
 {
+	clock_t start, end;
+	double result;
+	unsigned int a = 100;
+	unsigned int b = 30;
 
+	/*cout << "a : " << bitset<8>(a) << endl;
+	cout << "b : " << bitset<8>(b) << endl;
+	cout << "c : " << bitset<8>(c) << endl;*/
+
+	unsigned int c;
+	start = clock();
+	for (int i = 0; i < 256000; i++)
+	{
+		for (int j = 0; j < 256000; j++)
+		{
+			c = a + b;
+		}
+	}
+	result = clock() - start;
+	cout << "ê¸°ì¡´ ê±¸ë¦° ì‹œê°„ : " << result << endl;
+
+	unsigned int z;
+	start = clock();
+	for (int i = 0; i < 256000; i++)
+	{
+		for (int j = 0; j < 85330; j++)
+		{
+			z = ((a << 9 | a) << 9 | a) + ((b << 9 | b) << 9 | b);
+			z >> 18;
+			z << 14 >> 23;
+			z << 23 >> 23;
+		}
+	}
+
+	result = clock() - start;
+	cout << "ê¸°ì¡´ ê±¸ë¦° ì‹œê°„ : " << result << endl;
+
+	
+
+	system("pause");
+	return 0; // í”„ë¡œê·¸ëž¨ ë
+}
+
+void two()
+{
 	const unsigned int leftMask = 0xffff0000;
 	const unsigned int rightMask = 0x0000ffff;
-	cout << "        leftMaskºñÆ® : " << bitset<32>(rightMask) << endl;
-	cout << "        rightMaskºñÆ®: " << bitset<32>(leftMask) << endl;
+	cout << "        leftMaskë¹„íŠ¸ : " << bitset<32>(rightMask) << endl;
+	cout << "        rightMaskë¹„íŠ¸: " << bitset<32>(leftMask) << endl;
 
 	unsigned short a = 100;
 	unsigned short b = 60;
-	cout << "    a = 100 ºñÆ®·Î´Â : " << bitset<16>(a) << endl;
-	cout << "     b = 60 ºñÆ®·Î´Â : " << bitset<16>(b) << endl;
+	cout << "    a = 100 ë¹„íŠ¸ë¡œëŠ” : " << bitset<16>(a) << endl;
+	cout << "     b = 60 ë¹„íŠ¸ë¡œëŠ” : " << bitset<16>(b) << endl;
 
 	unsigned short c = 120;
 	unsigned short d = 70;
-	cout << "    c = 120 ºñÆ®·Î´Â : " << bitset<16>(c) << endl;
-	cout << "     d = 70 ºñÆ®·Î´Â : " << bitset<16>(d) << endl;
+	cout << "    c = 120 ë¹„íŠ¸ë¡œëŠ” : " << bitset<16>(c) << endl;
+	cout << "     d = 70 ë¹„íŠ¸ë¡œëŠ” : " << bitset<16>(d) << endl;
 
 	unsigned int y = (((unsigned int)a) << 16) | c;
 	unsigned int z = (((unsigned int)b) << 16) | d;
-	cout << "a.append(c) = y ºñÆ® : " << bitset<32>(y) << endl;
-	cout << "b.append(d) = z ºñÆ® : " << bitset<32>(z) << endl;
+	cout << "a.append(c) = y ë¹„íŠ¸ : " << bitset<32>(y) << endl;
+	cout << "b.append(d) = z ë¹„íŠ¸ : " << bitset<32>(z) << endl;
 
 	unsigned int result = y + z;
-	cout << " y + z = result ºñÆ® : " << bitset<32>(result) << endl;
+	cout << " y + z = result ë¹„íŠ¸ : " << bitset<32>(result) << endl;
 
 	unsigned short left = (result & leftMask) >> 16;
 	unsigned short right = result & rightMask;
-	cout << "   resultÀÇ leftºñÆ® : " << bitset<16>(left) << endl;
-	cout << "  resultÀÇ rightºñÆ® : " << bitset<16>(right) << endl;
+	cout << "   resultì˜ leftë¹„íŠ¸ : " << bitset<16>(left) << endl;
+	cout << "  resultì˜ rightë¹„íŠ¸ : " << bitset<16>(right) << endl;
 
 
 	cout << "a + b = " << left << endl;
 	cout << "c + d = " << right << endl;
-	system("pause");
-
-	return 0; // ÇÁ·Î±×·¥ ³¡
 }
